82f35522d29334cf1586405c934e38fa98e282ab
zhuangjie.1125 <zhuangjie.1125@bytedance.com>
2025-11-11 16:58:09 +0800

    feat: add libs for veauth

---
+++ libs/veauth/get_ark_api_key.go
@@ -0,0 +1,65 @@
+package veauth
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/eino-contrib/agentkit-ve/libs/veauth/internal"
+	"github.com/volcengine/volcengine-go-sdk/volcengine"
+	"github.com/volcengine/volcengine-go-sdk/volcengine/credentials"
+	"github.com/volcengine/volcengine-go-sdk/volcengine/session"
+)
+
+type option struct {
+	region string
+}
+type OptionFn func(*option)
+
+func WithRegion(region string) OptionFn {
+	return func(o *option) {
+		o.region = region
+	}
+}
+
+func GetArkAPIKey(ak, sk string, opts ...OptionFn) (string, error) {
+	opt := &option{
+		region: "cn-beijing",
+	}
+
+	for _, o := range opts {
+		o(opt)
+	}
+
+	config := volcengine.NewConfig().
+		WithRegion(opt.region).
+		WithCredentials(credentials.NewStaticCredentials(ak, sk, ""))
+	sess, err := session.NewSession(config)
+	if err != nil {
+		return "", err
+	}
+
+	arkSrv := internal.NewArkService(sess)
+	response, err := arkSrv.ListApiKeys(&internal.ListApiKeysInput{ProjectName: "default"})
+	if err != nil {
+		return "", err
+	}
+
+	if len(response.Items) == 0 {
+		return "", errors.New("list api keys returned empty list")
+	}
+
+	rawApiKeyOutput, err := arkSrv.GetRawApiKey(&internal.GetRawApiKeyInput{
+		Id: strconv.Itoa(int(response.Items[0].ID)),
+	})
+	if err != nil {
+		return "", err
+	}
+
+	apiKey := rawApiKeyOutput.ApiKey
+	if apiKey == nil {
+		return "", errors.New("get raw api key returned nil")
+	}
+
+	return *apiKey, nil
+
+}
---
+++ libs/veauth/go.mod
@@ -0,0 +1,10 @@
+module github.com/eino-contrib/agentkit-ve/libs/veauth
+
+go 1.14
+
+require github.com/volcengine/volcengine-go-sdk v1.1.47
+
+require (
+	github.com/volcengine/volc-sdk-golang v1.0.226 // indirect
+	golang.org/x/net v0.24.0 // indirect
+)
